119984c147f9fbe11d312d1981870285e443cc2a
yinqiao <1737666820@qq.com>
2021-12-21 12:41:51 +0800

    feat: 初始化模板

---
+++ .umirc.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from 'dumi'
+// import type WebpackChain from 'webpack-chain'
+
+export default defineConfig({
+  chainWebpack(memo) {
+    memo.plugins.delete('copy')
+  },
+  title: 'whooks',
+  favicon: 'https://avatars.githubusercontent.com/u/80503467?v=4',
+  logo: 'https://avatars.githubusercontent.com/u/80503467?v=4',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  alias: {
+    '@': './src',
+  },
+  includes: ['docs', 'packages/pkg/src'],
+  navs: [
+    null,
+    {
+      title: 'GitHub',
+      path: 'https://github.com/never-w/w-hooks.git',
+    },
+    { title: '帮助', path: 'https://www.baidu.com' },
+  ],
+  styles: ['https://cdnjs.cloudflare.com/ajax/libs/antd/4.17.3/antd.min.css'],
+  // menus: {
+  //   '/guide': [
+  //     {
+  //       title: '介绍',
+  //       path: '/guide/foo',
+  //     },
+  //     {
+  //       title: 'Blog',
+  //       path: '/guide/foo1',
+  //     },
+  //   ],
+  // },
+  locales: [['zh-CN', '中文']],
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+})
---
+++ packages/rnHooks/README.md
@@ -0,0 +1,11 @@
+# `rnHooks`
+
+> TODO: description
+
+## Usage
+
+```
+const rnhooks = require('rnHooks');
+
+// TODO: DEMONSTRATE API
+```
