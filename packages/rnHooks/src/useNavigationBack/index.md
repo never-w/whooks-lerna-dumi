95461619a78b265652db6a941b1faac5d65ae426
yinqiao <1737666820@qq.com>
2022-01-10 20:30:52 +0800

    feat: 新增useNavigationBack

---
+++ packages/rnHooks/src/useNavigationBack/index.md
@@ -0,0 +1,43 @@
+---
+title: useNavigationBack
+order: 6
+nav:
+  title: rnHooks
+  order: 2
+  path: /rnHooks/src
+group:
+  title: react-native-Hooks
+  path: /basis
+---
+
+# useNavigationBack
+
+自定义左上角返回按钮组件，并且自定义返回到哪个页面
+
+## 代码演示
+
+### 基础示例
+
+```typescript
+import { useNavigationBack } from 'w-rnhooks'
+
+useNavigationBack({
+  callback: () => {
+    navigation.navigate('PURCHASE_LIST', {
+      commodityType,
+    })
+  },
+  backNavigationElementFn: handleBackArrow => (
+    <CustomCom onPress={handleBackArrow} iconColor="#fff" />
+  ),
+})
+```
+
+### 参数
+
+| 参数                    | 说明                                                                                    | 类型                                                 | 默认值 |
+| ----------------------- | --------------------------------------------------------------------------------------- | ---------------------------------------------------- | ------ |
+| callback                | 可选，自定义点击返回/Android 触发物理按键返回的回调，默认返回上一页，用于返回自定义页面 | `VoidFunction`                                       | -      |
+| backNavigationElementFn | 自定义左上角返回按钮函数，参数`handleBackArrow`是你自定义的 callback                    | `(handleBackArrow: VoidFunction) => React.ReactNode` | -      |
+| resetHeaderBack         | 可选，是否重置左上角的返回按钮                                                          | `boolean`                                            | true   |
+| backShown               | 可选，是否隐藏返回按键                                                                  | `boolean`                                            | true   |
---
+++ packages/rnHooks/src/useNavigationBack/index.tsx
@@ -0,0 +1,71 @@
+import { useLayoutEffect, ReactElement } from 'react'
+import { BackHandler } from 'react-native'
+import { useNavigation, useIsFocused } from '@react-navigation/native'
+import { usePersistFn } from 'w-hooks'
+
+interface NavigationBackParams {
+  /** 自定义返回按键组件 */
+  backNavigationElementFn: (
+    handleBackArrow: VoidFunction,
+  ) => ReactElement<{ onPress: typeof handleBackArrow; iconColor: string }>
+  /** 点击返回/Android 触发物理按键返回的回调，默认返回上一页 */
+  callback?: () => void
+  /** 是否重置左上角的返回按钮 */
+  resetHeaderBack?: boolean
+  /** 是否隐藏返回按键 */
+  backShown?: boolean
+}
+
+const useNavigationBack = ({
+  backNavigationElementFn,
+  callback,
+  resetHeaderBack = true,
+  backShown = true,
+}: NavigationBackParams) => {
+  const navigation = useNavigation()
+  const isFocused = useIsFocused()
+
+  const onPressBackArrow = usePersistFn(() => {
+    if (callback) {
+      callback()
+    } else {
+      navigation.goBack()
+    }
+  })
+
+  useLayoutEffect(() => {
+    if (resetHeaderBack) {
+      navigation.setOptions({
+        headerLeft: () =>
+          backShown ? backNavigationElementFn(onPressBackArrow) : null,
+        gestureEnabled: false,
+      })
+    }
+
+    // Android 物理返回按钮
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        if (isFocused) {
+          onPressBackArrow()
+          return true
+        } else {
+          return false
+        }
+      },
+    )
+
+    return () => {
+      backHandler.remove()
+    }
+  }, [
+    resetHeaderBack,
+    onPressBackArrow,
+    navigation,
+    isFocused,
+    backShown,
+    backNavigationElementFn,
+  ])
+}
+
+export default useNavigationBack
